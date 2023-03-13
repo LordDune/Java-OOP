16de9e04d7684eb4580b777e94d25b6760db1129
Sergey LD <novatones1@gmail.com>
2023-03-13 19:40:52 +0500

    GeoTree++++

--- DZ/dz2/Geo/Main.java
+++ DZ/dz2/Geo/Main.java
@@ -21,18 +21,6 @@ public class Main  {
         gt.append(irina, Relationship.parent, jane, Relationship.children);
         gt.append(irina, Relationship.parent, ivan, Relationship.children);
         
-        // реализация поисков
-        // print(gt, vasya, Relationship.husband); // кому является вася мужем
-        // print(gt, irina, Relationship.parent); // кому является ирина родителем
-        // print(gt, masha, SearchRe.grandParent); // кому является ирина бабушкой
-        // print(gt, masha, Relationship.parent); // кому является ирина родителем
-        // print(gt, ivan, SearchRe.grandChildren); // кому является иван внуком
-        // print(gt, jane, SearchRe.grandChildren); // кому является женя внучкой
-        // print(gt, SearchAge.ageYounger, 40); // все люди до 40 лет
-        // print(gt, SearchAge.ageOlder, 40); // все люди от 40 лет
-        // print(gt, Gender.Male); // все люди мужского пола
-        // print(gt, Gender.Female); // все люди женского пола
-        
         //реализация интерфейса Printable
         gt.print(); // вывод информации о персонаже
         irina.print(); // вывод информации о древе
